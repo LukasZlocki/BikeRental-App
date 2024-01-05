72d659184b3fbf242acfc39aef80c0eeb9c01d77
lzlocki <lzlocki@poczta.fm>
2024-01-05 21:25:43 +0100

    Add method to send messages to all clients.

--- BikeRental.Api/Hubs/ChatHub.cs
+++ BikeRental.Api/Hubs/ChatHub.cs
@@ -8,5 +8,10 @@ namespace BikeRental.Api.Hubs
         {
             await Clients.All.SendAsync("ReceiveMessage", $"{Context.ConnectionId} has joined");
         }
+
+        public async Task SendMessage(string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", $"{Context.ConnectionId}: {message}");
+        }
     }
 }
